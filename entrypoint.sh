92dfec0c7b6f4a781d7a404be58569e47c9d9e13
Josh Rendek <josh@bluescripts.net>
2017-09-04 18:59:31 -0400

    Fix dockerfile issue with setup/travis

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /app
 export SECRET_KEY_BASE=$(bundle exec rails secret)
-bundle exec rake db:migrate
+SKIP_SETUP=true bundle exec rake db:migrate
 # super weird workaround for editor.md asset loading
 mkdir /app/bower_components/assets
 ln -s /app/bower_components/editor.md /app/bower_components/assets/editor.md
