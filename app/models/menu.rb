49335517a054e6305f6ca382d7e15b208f5ccb21
Josh Rendek <josh@bluescripts.net>
2017-09-04 19:10:41 -0400

    Bust cache after menus are updated

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,9 @@
 class Menu < ApplicationRecord
   default_scope -> { order(order: :asc) }
+
+  after_save :cache_buster
+
+  def cache_buster
+    FileUtils.rm_rf "#{Rails.root}/public/cached_pages"
+  end
 end
